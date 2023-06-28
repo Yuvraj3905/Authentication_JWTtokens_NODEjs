195880029a2b251ee609b63742f1da4a655797c2
yuvraj3905 <74104618+Yuvraj3905@users.noreply.github.com>
2023-06-28 23:46:59 +0530

    Completed With Code

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-const express=require('express');
-const jwt=require('jsonwebtoken');
-const app= express();
-const secretKey="secretKey";
-app.get("/",(req,res) => {
-    res.json({
-        message:"a sample api"
-    })
-})
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+const secretKey = "secretKey";
 
-app.post("/login",(req,res) => {
-    const user={
-        id:1,
-        username: "yuvi",
-        email: "yuvraj202001@gmail.com"
+app.get("/", (req, res) => {
+  res.json({
+    message: "A sample API"
+  });
+});
+
+app.post("/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "yuvi",
+    email: "yuvraj202001@gmail.com"
+  };
+
+  jwt.sign({ user }, secretKey, { expiresIn: '300s' }, (err, token) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.json({ token });
     }
-    jwt.sign({user},secretKey,{expiresIn:'300s'},(err,token) => {
-      res.json({
-        token
-      })
-    });
-})
+  });
+});
 
-app.listen(5000,()=>{
-    console.log("app running on 5000");
-})
+app.post("/profile", verifyToken, (req, res) => {
+  jwt.verify(req.token, secretKey, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "Access granted",
+        data: authData
+      });
+    }
+  });
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearerToken = bearerHeader.split(' ')[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+app.listen(5000, () => {
+  console.log("App running on port 5000");
+});
